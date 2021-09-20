bfc8f6ed82b40ad8623059b09c216f16069d30a8
raiguard <caleb.heuer@gmail.com>
2021-09-19 18:33:55 -0600

    Make kak_print a macro

--- src/util.rs
+++ src/util.rs
@@ -3,11 +3,17 @@ pub fn editor_escape(s: &str) -> String {
     s.replace("'", "''")
 }
 
-pub fn kak_print(s: &str) {
-    println!("echo -debug '{}'", editor_escape(&s))
-}
-
 /// Strips a string of all non-alphanumeric characters
 pub fn strip_an(s: &str) -> String {
     s.to_owned().replace(|c: char| !c.is_alphanumeric(), "")
+}
+
+/// Prints to the kakoune debug log, using the same syntax as `println!`.
+macro_rules! kak_print {
+    ($literal:expr) => {
+        println!("echo -debug '{}'", $literal)
+    };
+    ($template:expr, $($arg:tt)*) => ({
+        println!("echo -debug '{}'", format!($template, $($arg)*));
+    })
 }
